b03e943ab63ed2de4a146934e2613fdd24bc9996
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-10-28 17:53:30 -0300

    re-do & fixes

--- frontend/front.js
+++ frontend/front.js
@@ -1,149 +1,130 @@
-const getUsers = async () => {
+
+async function fetchData(url, options = {}) {
     try {
-        const response = await fetch("http://localhost:5000/users/", {
-            method: "GET",
-        });
-
+        const response = await fetch(url, options);
         if (!response.ok) {
-            throw Error;
+            throw new Error(response.statusText);
         }
-
-        const result = await response.json();
-
-        // console.log('getUsers', result);
-        return result;
+        return await response.json();
     } catch (error) {
-        console.log(error);
+        console.error(error);
+        throw error;
     }
-};
+}
 
-function populateTable(users) {
-    const front = document.querySelector("#front");
-    // console.log('Dentro: ', users);
+function populateTable(user) {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.age}</td>
+        <td>
+            <button class="btn btn-warning" data-bs-id="${user.id}">
+                <i class="bi bi-pencil-square"></i>
+                 Editar
+            </button>
+            <button class="btn btn-danger" data-bs-id="${user.id}">
+                <i class="bi bi-trash"></i>
+                Eliminar
+            </button>
+        </td>
+    `;
 
-    users.forEach((usuario) => {
-        const fila = document.createElement("tr");
-        fila.innerHTML = `
-            <td>${usuario.id}</td>
-            <td>${usuario.name}</td>
-            <td>${usuario.age}</td>
-            <td>
-                <button class="btn btn-warning" id="botonEditar" onclick="openModalEditar(${usuario.id},'${usuario.name}',${usuario.age})" data-bs-id=${usuario.id}">
-                    <i class="bi bi-pencil-square"></i>
-                     Editar
-                </button>
-                <button class="btn btn-danger" id="botonEliminar" onclick="HandlerEliminar(${usuario.id})">
-                    <i class="bi bi-trash"></i>
-                     Eliminar
-                </button>
-            </td>
-        `;
-        front.appendChild(fila);
-    });
+    const editButton = fila.querySelector(".btn-warning");
+    const deleteButton = fila.querySelector(".btn-danger");
+
+    editButton.addEventListener("click", () => openModalEditar(user));
+    deleteButton.addEventListener("click", () => HandlerEliminar(user.id));
+
+    return fila;
+}
+
+function openModalEditar(user) {
+    const nameInput = document.querySelector("#name");
+    const ageInput = document.querySelector("#age");
+    const modal = $("#modalUsuario");
+    const guardarButton = document.querySelector("#botonGuardar");
+
+    nameInput.value = user.name;
+    ageInput.value = user.age;
+    guardarButton.dataset.userId = user.id;
+
+    modal.modal("show");
 }
 
 function openModalAgregar() {
-    $("#modalUsuario").modal("show");
-    const botonGuardar = document.querySelector("#botonGuardar");
-    botonGuardar.addEventListener("click", async () => {
-        let name = document.querySelector("#name");
-        let age = document.querySelector("#age");
-        const user = {
-            name: name.value,
-            age: age.value,
+    const nameInput = document.querySelector("#name");
+    const ageInput = document.querySelector("#age");
+    const modal = $("#modalUsuario");
+    const guardarButton = document.querySelector("#botonGuardar");
+
+    nameInput.value = '';
+    ageInput.value = '';
+    delete guardarButton.dataset.userId;
+
+    modal.modal("show");
+}
+
+async function loadUsers() {
+    const front = document.querySelector("#front");
+    try {
+        const users = await fetchData("http://localhost:5000/users");
+        front.innerHTML = ""; // Limpiar la tabla antes de agregar los datos
+        users.forEach((user) => {
+            front.appendChild(populateTable(user));
+        });
+    } catch (error) {
+        console.error("Error al cargar usuarios:", error);
+    }
+}
+
+// Función para guardar un usuario (ya sea crear o editar)
+async function saveUser(user) {
+    try {
+        const requestOptions = {
+            method: user.id ? "PUT" : "POST", // Si tiene ID, es una actualizacion; de lo contrario, es una creacion
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
         };
-        const result = await postUser(user);
-        console.log(result);
-        $("#modalCrearUsuario").modal("hide");
-        window.location.reload();
+
+        await fetchData(`http://localhost:5000/users/${user.id || ""}`, requestOptions);
+
+        const modal = $("#modalUsuario");
+        modal.modal("hide");
+        loadUsers();
+    } catch (error) {
+        console.error("Error al guardar usuario:", error);
+    }
+}
+
+async function HandlerEliminar(userId) {
+    try {
+        await fetchData(`http://localhost:5000/users/${userId}`, { method: "DELETE" });
+        loadUsers();
+    } catch (error) {
+        console.error("Error al eliminar usuario:", error);
+    }
+}
+
+//* Inicializar la aplicación
+function initializeApp() {
+    const guardarButton = document.querySelector("#botonGuardar");
+    guardarButton.addEventListener("click", () => {
+        const nameInput = document.querySelector("#name");
+        const ageInput = document.querySelector("#age");
+        const userId = guardarButton.dataset.userId;
+        const user = { id: userId, name: nameInput.value, age: ageInput.value };
+        saveUser(user);
     });
+
+    const agregarButton = document.querySelector("#abrirModalAgregar");
+    agregarButton.addEventListener("click", openModalAgregar);
+
+    //* Cargar usuarios al iniciar la aplicación
+    loadUsers();
 }
 
-async function postUser(user) {
-    try {
-        const response = await fetch("http://localhost:5000/users/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-        });
-        const result = await response.json();
-        console.log("Ok: ", result);
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-}
-
-
-async function HandlerEliminar(userid) {
-    try {
-        const response = await fetch("http://localhost:5000/users/" + userid, { method: 'DELETE' });
-        console.log("Ok: ", response);
-        window.location.reload();
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-
-}
-
-
-async function editartUser(user) {
-    try {
-        const response = await fetch("http://localhost:5000/users/" + user.id, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-        });
-        const result = await response.json();
-        console.log("Ok: ", result);
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-}
-
-
-async function openModalEditar(idusuario, updname, updage) {
-    try {
-        $("#modalUsuario").modal("show");
-        console.log(updname);
-        let name = document.querySelector("#name");
-        let age = document.querySelector("#age");
-
-        name.value = updname;
-        age.value = updage;
-
-        const botonGuardar = document.querySelector("#botonGuardar");
-
-        botonGuardar.addEventListener("click", async () => {
-            let name = document.querySelector("#name");
-            let age = document.querySelector("#age");
-
-            const user = {
-                id: idusuario,
-                name: name.value,
-                age: age.value,
-            };
-
-            const result = await editartUser(user);
-            console.log(result);
-            $("#modalCrearUsuario").modal("hide");
-            window.location.reload();
-        });
-
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-
-}
-
-
-async function app() {
-    const users = await getUsers();
-    populateTable(users);
-}
-
-
-app();
+//* Inicializar la aplicación cuando se carga la página
+document.addEventListener("DOMContentLoaded", initializeApp);
